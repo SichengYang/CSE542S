009844a25505e9e220cf9d684f27ebcd72236b4f
SichengYang <sicheng@wustl.edu>
2024-09-23 16:13:34 -0500

    lab1 draft

--- lab1/main.rs
+++ lab1/main.rs
@@ -8,15 +8,15 @@ fn usage(program_name: &String){
 }
 
 fn parse_args(config_name: & mut String) -> Result<(), u8>{
-    let mut v = Vec::new();
-        for arg in env::args(){
+    	let mut v = Vec::new();
+	for arg in env::args(){
 		v.push(arg);
-        }
+	}
 
 	if v.len() < MIN || v.len() > MAX{
 		usage(&v[PROGRAM_NAME_INDEX]);
 		return Err(FAIL_BAD_COMMANDLINE);
-        }
+	}
 
 	if v.len()==MAX{
 		if v[OPTIONS_INDEX] != "whinge".to_string(){
@@ -26,26 +26,35 @@ fn parse_args(config_name: & mut String) -> Result<(), u8>{
 			COMPLAIN.store(true, atomic::Ordering::SeqCst);
 		}
 
-        }
+	}
 
 	*config_name = v[CONFIG_FILE_INDEX].clone();
-	
+
 
 	Ok(())
 
 }
 
 fn recite(title: &String, play: &Play){
-    println!("Title of the play: {}", title);
+	println!("Title of the play: {}", title);
+	let mut current_character: String = "".to_string();
 
-    for tuple in play{
-        match tuple{(.., character, line)=> println!("{}: {}", character, line)};
-    }
+	for tuple in play{
+		match tuple{
+			(.., character, line)=> {
+				if !character.eq(&current_character){
+					println!();
+					println!("{}.", character);
+					current_character = character.clone();
+				}
+				println!("{}", line);
+			}			
+		}
+	}
 
 }
+
 fn main() -> Result<(), u8> {
-    	println!("Hello, world!");
-	
 	let mut config_file: String = "".to_string();
 	let parse_result = parse_args(&mut config_file);
 	match parse_result {
@@ -53,10 +62,16 @@ fn main() -> Result<(), u8> {
 		_ => {}
 	}
 	
-	let mut play_name: String = "".to_string();
+	let mut title: String = "".to_string();
 	let mut play: Play = vec!();
 
+	match script_gen(&mut title, &mut play, &config_file){
+		Err(e) => return Err(e),
+		_ => {
+			play.sort_by(|a, b| (a.0).cmp(&b.0));
+			recite(&title, &play);	
+		}
+	}
 	
-
     	Ok(())
 }
--- lab1/script_gen.rs
+++ lab1/script_gen.rs
@@ -5,17 +5,19 @@ use std::io::BufRead;
 type PlayConfig = Vec<(String, String)>;
 
 const TITLE: usize = 0;
+const SKIP_TITLE:usize = 1;
 const CHARACTER_NAME: usize = 1;
 const CHARACTER: usize = 0;
 const CHARACTER_FILE: usize = 1;
 const TOKEN_NUM: usize = 2;
 
+const END_OF_FILE: usize = 0;
 const PARSE_FAIL: u8 = 2;
 const INVALID_FILENAME: u8 = 3;
 const FILE_READ_ERR: u8 = 4;
 
-fn add_script_line(play: &mut Play, line: &String, name: &String) {
-	if line.len() > 0{
+fn add_script_line(play: &mut Play, line: &String, name: &String) {	
+	if !line.is_empty() {
 		if let Some((first_token, rest_of_line)) = line.split_once(char::is_whitespace){
 			let first_token_trim = first_token.trim();
 			let rest_of_line_trim = rest_of_line.trim();
@@ -50,7 +52,7 @@ fn grab_trimmed_file_lines(filename: &String, lines: &mut Vec<String>) -> Result
 	loop {
 		buffer.clear();
 		match reader.read_line(&mut buffer) {
-			Ok(0) => return Ok(()),
+			Ok(END_OF_FILE) => return Ok(()),
 			Ok(_) => {
 				lines.push(buffer.trim().to_string());	
 			},
@@ -60,10 +62,11 @@ fn grab_trimmed_file_lines(filename: &String, lines: &mut Vec<String>) -> Result
 			}
 		}	
 	}
+	
 }
 
-fn process_config(play: &mut Play, config: &PlayConfig) -> Result<(), u8> {
-	for element in config {
+fn process_config(play: &mut Play, config: &PlayConfig) -> Result<(), u8> {	
+	for element in config {		
 		match element {
 			(character_name, text_file) => {
 				let mut lines: Vec<String> = Vec::new();
@@ -71,7 +74,7 @@ fn process_config(play: &mut Play, config: &PlayConfig) -> Result<(), u8> {
 					Err(e) => {
 						return Err(e);
 					},
-					_ => {
+					_ => {					
 						for line in lines {
 							add_script_line(play, &line, &character_name);						
 						}
@@ -82,8 +85,46 @@ fn process_config(play: &mut Play, config: &PlayConfig) -> Result<(), u8> {
 		}
 		
 	}
+
 	Ok(())
 }
 
 
+fn add_config(line: &String, config: &mut PlayConfig){
+	let mut v: Vec<&str> = line.split_whitespace().collect();	
+	if v.len() != TOKEN_NUM{
+		if COMPLAIN.load(atomic::Ordering::SeqCst) {				
+			println!("Config file line length not equal to 2");
+		}
+	}else{
+		config.push((v[CHARACTER].to_string(), v[CHARACTER_FILE].to_string()));
+	}
 
+}
+
+fn read_config(file_name: &String, title:&mut String, config: &mut PlayConfig) -> Result<(), u8> {
+	let mut lines:Vec<String> = Vec::new();
+	match grab_trimmed_file_lines(file_name, &mut lines){
+		Err(e) => return Err(e),
+		Ok(_) => {
+			*title = lines[TITLE].clone();
+			for line in lines.iter().skip(SKIP_TITLE){
+				add_config(line, config);
+			}
+		}
+	}
+	Ok(())
+}
+
+fn script_gen(title:&mut String, play: &mut Play, file_name: &String) -> Result<(), u8>{
+	let mut play_config: PlayConfig = vec![];
+	match read_config(file_name, title, &mut play_config){
+  		Err(e) => return Err(e),
+		_ => {	
+			match process_config(play, &play_config){
+				Err(e) => return Err(e),
+				_ => { return Ok(()); }
+			}
+		} 
+	}
+}
