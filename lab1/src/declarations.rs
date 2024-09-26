f780c530058092d46158ff874277db32c232e9a7
Nick Song <112423678+qinzhounick@users.noreply.github.com>
2024-09-25 23:24:26 -0500

    Update declarations.rs

--- lab1/src/declarations.rs
+++ lab1/src/declarations.rs
@@ -1,14 +1,14 @@
 use std::sync::atomic;
 
-type Play = Vec<(usize, String, String)>;
+type Play = Vec<(usize, String, String)>; //declare Play vector to store line number, character, line
 
-const MIN: usize = 2;
-const MAX: usize = 3;
-const PROGRAM_NAME_INDEX: usize = 0;
-const CONFIG_FILE_INDEX: usize = 1;
-const OPTIONS_INDEX: usize = 2;
+const MIN: usize = 2; //need at least two command line arguments
+const MAX: usize = 3; //cannot have more than three command line arguments
+const PROGRAM_NAME_INDEX: usize = 0; //progarm name index
+const CONFIG_FILE_INDEX: usize = 1; //config file index
+const OPTIONS_INDEX: usize = 2; //whinge option index
 
-const SUCCESS: u8 = 0;
-const FAIL_BAD_COMMANDLINE: u8 = 1;
-const FAIL_GENERATE_SCRIPT: u8 = 2;
-static COMPLAIN: atomic::AtomicBool = atomic::AtomicBool::new(false);
+
+const FAIL_BAD_COMMANDLINE: u8 = 1; //return value for bad command line
+const FAIL_GENERATE_SCRIPT: u8 = 2; //return value for fail to generate script
+static COMPLAIN: atomic::AtomicBool = atomic::AtomicBool::new(false); //atomic bool indicating if whinge is set or not
