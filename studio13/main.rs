cbbe97a21b6c22e9be2646b50fb274c185bdf0bb
Sicheng Yang <sicheng@Sichengs-MacBook-Air.local>
2024-10-22 10:52:05 -0500

    1

--- studio13/main.rs
+++ studio13/main.rs
@@ -1,5 +1,30 @@
-
+struct S<T>{
+    x: T,
+    y: fn(T) -> T,
+    z: fn(T) -> T
+}
 
 fn main() {
-    
+    let x: f64 = 0.0;
+    let cos_sin = |x| {
+        println!("cos: {}", f64::cos(f64::sin(x)));
+    };
+
+    cos_sin(x);
+
+    let s = S::<f64>{x: 0.0, y: f64::cos, z: f64::sin};
+    let s_closure = |s: S<f64>| {
+        println!("cos: {}", (s.y)((s.z)(s.x)));
+    };
+
+    s_closure(s);
+
+    let mut string = "Hello".to_string();
+    let str_closure = |mut s: String| -> String {
+        s = s + ", world!";
+        s
+    };
+
+    println!("orinigal string: {}", string);
+    println!("string closure: {}", str_closure(string));
 }
