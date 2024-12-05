3a3ca8ad776ce8c797b970721060f7bde9d40c04
SichengYang <sicheng@wustl.edu>
2024-12-05 17:59:14 -0600

    fix reading error

--- lab3client/src/lab3/declarations.rs
+++ lab3client/src/lab3/declarations.rs
@@ -5,4 +5,10 @@ pub const FAIL_GENERATE_SCRIPT: u8 = 2; //return value for fail to generate scri
 pub const FAIL_CONCURRENCY: u8 = 3; //return value for concurrency hazard
 pub const FAIL_CONNECTION: u8 = 4; //return value for Tcpstream connection fail
 pub const FAIL_OPEN_FILE: u8 = 5; //return value for fail to open fail sent by server
+pub const INTERNET_ERROR: u8 = 6;
+pub const SERVER_FAILED: u8 = 7;
+
+pub const END_OF_FILE: usize = 0;
+pub const SUCCESS_MESSAGE: &str = "HTTP/1.1 200";
+
 pub static COMPLAIN: atomic::AtomicBool = atomic::AtomicBool::new(false); //atomic bool indicating if whinge is set or not
--- lab3client/src/lab3/script_gen.rs
+++ lab3client/src/lab3/script_gen.rs
@@ -1,29 +1,18 @@
 use super::declarations::FAIL_GENERATE_SCRIPT;
 use super::declarations::FAIL_CONNECTION;
 use super::declarations::FAIL_OPEN_FILE;
+use super::declarations::INTERNET_ERROR;
+use super::declarations::SERVER_FAILED;
+use super::declarations::END_OF_FILE;
+use super::declarations::SUCCESS_MESSAGE;
+
 use std::fs::File;
 use std::io::{BufReader, BufRead, Write, Read};
 use std::net::TcpStream;
 
-const END_OF_FILE: usize = 0;
-
 //grab_trimmed_file_lines
 //  read trimmed lines from files
 pub fn grab_trimmed_file_lines(filename: &String, lines: &mut Vec<String>) -> Result<(), u8> {
-	let file = match File::open(filename) {  //check if the file opens successfully
-		Ok(f) => {  //if yes, store file
-			f
-		},
-		Err(_) => {  //if not, print message and return error
-			let result = writeln!(std::io::stderr().lock(), "\t --Warning: {} is not a valid filename", filename);
-			match result {
-				Err(e) => println!("Writeln error with {e}"),
-				_ => {}
-			}
-			return Err(FAIL_GENERATE_SCRIPT);
-		}
-	};
-
 	let mut reader = match get_buffered_reader(filename){//create bufreader to read from file
 		Ok(reader) => reader,
 		Err(e) => return Err(e)
@@ -38,7 +27,7 @@ pub fn grab_trimmed_file_lines(filename: &String, lines: &mut Vec<String>) -> Re
 				lines.push(buffer.trim().to_string());	//add the trimmed line to lines
 			},
 			Err(_) => {  //if returned error, print message and return error
-				let result = writeln!(std::io::stderr().lock(), "\t --Warning: This file cannot be read");
+				let result: Result<(), std::io::Error> = writeln!(std::io::stderr().lock(), "\t --Warning: This file cannot be read");
 				match result {
 					Err(e) => println!("Writeln error with {e}"),
 					_ => {}
@@ -53,7 +42,7 @@ pub fn grab_trimmed_file_lines(filename: &String, lines: &mut Vec<String>) -> Re
 
 pub fn get_buffered_reader(message: &String) -> Result<BufReader<Box<dyn Read>>, u8> {
     if let Some(addr) = message.strip_prefix("net:") {
-        let addr: Vec<&str> = message.splitn(3, ':').collect();
+        let addr: Vec<&str> = addr.splitn(3, ':').collect();
         if addr.len() == 3 {
             let addr_port = addr[0].to_string() + ":" + addr[1];
             let file_name = addr[2];
@@ -72,10 +61,21 @@ pub fn get_buffered_reader(message: &String) -> Result<BufReader<Box<dyn Read>>,
 						Ok(_) => {}
 					}
 
-					let reader = BufReader::new(Box::new(stream) as Box<dyn Read>);
-                    return Ok(reader);
+					let mut reader = BufReader::new(Box::new(stream) as Box<dyn Read>);
+					let mut status = String::new();
+					match reader.read_line(&mut status){
+						Err(_) => return Err(INTERNET_ERROR), // The response must have a status code or the internet is not stable
+						Ok(_) => {
+							println!("{status}");
+							if status.trim() == SUCCESS_MESSAGE.to_string() {
+								return Ok(reader);
+							} else{
+								return Err(SERVER_FAILED);
+							}
+						}
+					}
                 }
-                Err(e) => {
+                Err(_) => {
                     return Err(FAIL_CONNECTION);
                 }
             }
@@ -84,6 +84,6 @@ pub fn get_buffered_reader(message: &String) -> Result<BufReader<Box<dyn Read>>,
 
     match File::open(message) {
         Ok(file) => {return Ok(BufReader::new(Box::new(file) as Box<dyn Read>));} ,
-        Err(e) => {return Err(FAIL_OPEN_FILE);}
+        Err(_) => {return Err(FAIL_OPEN_FILE);}
     }
 }
