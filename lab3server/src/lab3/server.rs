dab0f53707220b48039194827f62c07bfc93404b
Nick Song <112423678+qinzhounick@users.noreply.github.com>
2024-12-05 11:23:53 -0600

    Update server.rs

--- lab3server/src/lab3/server.rs
+++ lab3server/src/lab3/server.rs
@@ -3,7 +3,10 @@
 // This file contains the definitions of server. It will recieve the request from client and server requested file
 
 // import field
-use std::net::TcpListener;
+use std::net::{TcpListener, Shutdown};
+use regex::Regex;
+use std::fs;
+use std::fs::File;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::SeqCst;
 use std::thread;
@@ -65,14 +68,14 @@ impl Server{
                 Some(valid_listener) => {
                     let request = valid_listener.accept();
                     match request { // the expect will not happen because we have checked
-                        Ok((mut _socket, _addr)) => {
+                        Ok((mut socket, _addr)) => {
                             if CANCEL_FLAG.load(SeqCst) {
                                 return;
                             }
 
                             let _handle = thread::spawn(move || {
                                 let mut buffer = [BUFFER_INITIAL; BUFFER_SIZE];
-                                match _socket.read(&mut buffer){
+                                match socket.read(&mut buffer){
                                     Err(_) => {
                                         let result = writeln!(std::io::stderr().lock(), "read error with {_addr}");
 
@@ -84,9 +87,51 @@ impl Server{
                                     Ok(bytes_read) => {
                                         let body = String::from_utf8_lossy(&buffer[..bytes_read]);
                                         println!("Received: {}", body);
+                                        if body=="quit" {
+                                            CANCEL_FLAG.store(true, SeqCst);
+                                        }else{
 
+                                            let filename = body.to_string();
+                                            let re = Regex::new(r"[\$\\/]|(\.\.)").unwrap();
+                                            if re.is_match(filename){
+                                                return;
+                                            }
+
+                                            let file = match File::open(filename.clone()) {  //check if the file opens successfully
+                                                Ok(f) => {  //if yes, store file
+                                                    f
+                                                },
+                                                Err(_) => {  //if not, print message and return error
+                                                    let result = writeln!(std::io::stderr().lock(), "\t --Warning: {} is not a valid filename", filename);
+                                                    match result {
+                                                        Err(e) => println!("Writeln error with {e}"),
+                                                        _ => {}
+                                                    }
+                                                    socket.shutdown(Shutdown::Both).expect("Shutdown failed");
+                                                    return;
+                                                }
+                                            };
+                                            let buffer = match fs::read(filename){
+                                                Err(_) => {
+                                                    let result = writeln!(std::io::stderr().lock(), "\t --Warning: {} is not a valid filename", filename);
+                                                    match result {
+                                                        Err(e) => println!("Writeln error with {e}"),
+                                                        _ => {}
+                                                    }
+                                                    
+                                                    return;
+                                                },
+                                                (data) => {
+                                                    data
+                                                }
+                                            }
+
+
+                                        }
+
+                                        
                                         let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
-                                        match _socket.write_all(response.as_bytes()) {
+                                        match socket.write_all(response.as_bytes()) {
                                             Err(_) => {
                                                 let result = writeln!(std::io::stderr().lock(), "response write error with {_addr}");
 
@@ -114,4 +159,19 @@ impl Server{
             }
         }
     }
+}
+
+fn respondToSocket(){
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+    match socket.write_all(response.as_bytes()) {
+        Err(_) => {
+            let result = writeln!(std::io::stderr().lock(), "response write error with {_addr}");
+
+            match result {
+                Err(write_e) => println!("Writeln error with {write_e:?}"),
+                _ => {}
+            }
+        },
+        Ok(_) => {}
+    };
 }
