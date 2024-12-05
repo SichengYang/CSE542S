f929683898805d0db18b452da47a4a42c169e5a0
SichengYang <sicheng@wustl.edu>
2024-12-04 23:26:42 -0600

    lab3 q9

--- lab3server/src/lab3/server.rs
+++ lab3server/src/lab3/server.rs
@@ -5,10 +5,15 @@
 // import field
 use std::net::TcpListener;
 use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::SeqCst;
+use std::thread;
+use std::io::{Write, Read};
 
 // constant
 const SUCCESS: bool = true;
 const SERVER_START_FAILED: bool = false;
+const BUFFER_SIZE: usize = 128;
+const BUFFER_INITIAL: u8 = 0;
 
 static CANCEL_FLAG: AtomicBool = AtomicBool::new(false);
 
@@ -39,7 +44,7 @@ impl Server{
         let listener = TcpListener::bind(address);
 
         match listener{
-            Err(e) => return SERVER_START_FAILED,
+            Err(_) => return SERVER_START_FAILED,
             Ok(valid_listener) => {
                 self.listener = Some(valid_listener);
                 self.listening_addr = address.to_string();
@@ -48,5 +53,65 @@ impl Server{
         }
     }
 
-    
+    pub fn run(&self) {
+        loop {
+            // detect the stop point of server
+            if CANCEL_FLAG.load(SeqCst) {
+                return;
+            }
+            
+            match &self.listener {
+                None => return,
+                Some(valid_listener) => {
+                    let request = valid_listener.accept();
+                    match request { // the expect will not happen because we have checked
+                        Ok((mut _socket, _addr)) => {
+                            if CANCEL_FLAG.load(SeqCst) {
+                                return;
+                            }
+
+                            let _handle = thread::spawn(move || {
+                                let mut buffer = [BUFFER_INITIAL; BUFFER_SIZE];
+                                match _socket.read(&mut buffer){
+                                    Err(_) => {
+                                        let result = writeln!(std::io::stderr().lock(), "read error with {_addr}");
+
+                                        match result {
+                                            Err(write_e) => println!("Writeln error with {write_e:?}"),
+                                            _ => {}
+                                        }
+                                    },
+                                    Ok(bytes_read) => {
+                                        let body = String::from_utf8_lossy(&buffer[..bytes_read]);
+                                        println!("Received: {}", body);
+
+                                        let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+                                        match _socket.write_all(response.as_bytes()) {
+                                            Err(_) => {
+                                                let result = writeln!(std::io::stderr().lock(), "response write error with {_addr}");
+
+                                                match result {
+                                                    Err(write_e) => println!("Writeln error with {write_e:?}"),
+                                                    _ => {}
+                                                }
+                                            },
+                                            Ok(_) => {}
+                                        };
+                                    }
+                                }
+                            });
+                        }
+                        Err(network_e) => {
+                            let result = writeln!(std::io::stderr().lock(), "Connection error with {network_e:?}");
+
+                            match result {
+                                Err(write_e) => println!("Writeln error with {write_e:?}"),
+                                _ => {}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
